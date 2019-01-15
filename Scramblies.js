1c9df2bb6ba59e847b659f26d8f0894d59d8cb1a
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-01-15 16:50:58 +0700

    Create Scramblies.js

---
+++ Scramblies.js
@@ -0,0 +1,19 @@
+/*
+Description:
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+Notes:
+
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered
+Input strings s1 and s2 are null terminated.
+Examples
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+*/
+
+function scramble(str1, str2) {
+  let occurences = str1.split("").reduce((arr, cur) => { arr[cur] ? arr[cur]++ : arr[cur] = 1; return arr; }, {});
+  return str2.split("").every((character) => --occurences[character] >= 0);
+}
