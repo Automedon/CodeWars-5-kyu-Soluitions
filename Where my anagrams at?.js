cf42760c43d752ec25efee70f374cd773107428b
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-01-24 16:30:01 +0700

    Create Where my anagrams at?.js

---
+++ Where my anagrams at?.js
@@ -0,0 +1,22 @@
+/*
+Description:
+What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+'abba' & 'baab' == true
+
+'abba' & 'bbaa' == true
+
+'abba' & 'abbba' == false
+
+'abba' & 'abca' == false
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+*/
+function anagrams(word, words) {
+  return words.filter(v=>v.split('').sort().join('')===word.split('').sort().join(''))
+}
